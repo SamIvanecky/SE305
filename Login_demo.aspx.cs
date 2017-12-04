62b5ea6d6f0a1d016181c753ca3b14352f9587fc
Sam Ivanecky <31965812+SamIvanecky@users.noreply.github.com>
2017-12-03 21:19:06 -0600

    Add files via upload

---
+++ Login_demo.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Login_demo : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=sam-laptop\sqlexpress;Integrated Security=True");
+        SqlCommand cmd = new SqlCommand("select * from Logins where username=@username and password=@password", con);
+        cmd.Parameters.AddWithValue("@username", TextBox1.Text);
+        cmd.Parameters.AddWithValue("password", TextBox2.Text);
+        SqlDataAdapter sda = new SqlDataAdapter(cmd);
+        DataTable dt = new DataTable();
+        sda.Fill(dt);
+        con.Open();
+        int i = cmd.ExecuteNonQuery();
+        con.Close();
+
+        if (dt.Rows.Count > 0)
+        {
+            Response.Redirect("Redirect_page.aspx");
+        }
+        else
+        {
+            Label1.Text = "Your username and password is incorrect";
+            Label1.ForeColor = System.Drawing.Color.Red;
+
+        }
+
+
+    }
+}
